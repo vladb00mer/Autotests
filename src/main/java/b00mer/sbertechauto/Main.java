2ef2401fe27854e603a45e993e8117bac5bc60eb
b00mer <b00mer@vladb00mer>
2016-01-31 10:18:54 +0300

    first commit

---
+++ src/main/java/b00mer/sbertechauto/Main.java
@@ -0,0 +1,11 @@
+package b00mer.sbertechauto;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        
+
+    }
+    
+}
---
+++ src/test/java/b00mer/sbertechauto/autotests/GodTest.java
@@ -0,0 +1,30 @@
+package b00mer.sbertechauto.autotests;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class GodTest {
+    
+    public GodTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+}
