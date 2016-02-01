911fac36d19841ba011bb76d402deeca819516d8
b00mer <b00mer@vladb00mer>
2016-02-01 12:38:35 +0300

    func add

--- src/test/java/b00mer/sbertechauto/autotests/GodTest.java
+++ src/test/java/b00mer/sbertechauto/autotests/GodTest.java
@@ -9,15 +9,17 @@ import static org.junit.Assert.*;
 
 public class GodTest {
     
-    ArrayList<Expression> expressionList = new ArrayList();
+    ArrayList<Expression> expressionList;
     
     public GodTest() {
+        
+        expressionList = new ArrayList();
     }
     
     @Before
     public void setUp() {
         
-        expressionList.add(new Expression ("1","1","+","2"));
+        expressionList.add(new Expression ("1","2","+","3"));
         expressionList.add(new Expression ("2","1","-","1"));
         expressionList.add(new Expression ("2","2","*","4"));
         expressionList.add(new Expression ("6","2","/","3"));
@@ -31,7 +33,7 @@ public class GodTest {
     
     @Test
     public void test() {
-    
+        
         expressionList.stream().forEach((exp) -> {
             assertTrue (exp.getLocalResult() == exp.getResult());
         });
