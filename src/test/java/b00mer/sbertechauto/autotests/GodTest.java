4a5a6bb9ba9a12bdf6b74259abc541a71b8152eb
b00mer <b00mer@vladb00mer>
2016-02-01 11:14:30 +0300

    first test check ok

--- src/test/java/b00mer/sbertechauto/autotests/GodTest.java
+++ src/test/java/b00mer/sbertechauto/autotests/GodTest.java
@@ -2,31 +2,22 @@ package b00mer.sbertechauto.autotests;
 
 import b00mer.sbertechauto.Expression;
 import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class GodTest {
     
-    Expression expression;
+    Expression expression, expression1;
     
     public GodTest() {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() { 
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
     }
     
     @Before
     public void setUp() {
         
-        expression = new Expression ("1","2","+","4");
+        expression = new Expression ("1","2","+","3");
+        expression1 = new Expression ("1","2","+","5");
     }
     
     @After
@@ -36,6 +27,7 @@ public class GodTest {
     }
     @Test
     public void test() {
-    assertEquals (1,2);
+    assertTrue (expression.getLocalResult() == expression.getResult());
+    assertFalse (expression1.getLocalResult() == expression1.getResult());
     }    
 }
