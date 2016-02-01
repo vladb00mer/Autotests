e38cb7e39cd2025693d64560a851daa2fc06f373
b00mer <b00mer@vladb00mer>
2016-02-01 10:08:20 +0300

    almost finished

--- src/main/java/b00mer/sbertechauto/Expression.java
+++ src/main/java/b00mer/sbertechauto/Expression.java
@@ -2,32 +2,46 @@ package b00mer.sbertechauto;
 
 public class Expression {
 
+//variable declaration
 String operand1, operand2, operation, result = new String();
+Double gotResult;
 
+//default constructor
 public Expression() {}
 
+//overloaded constructor
 public Expression(String operand1, String operand2, String operation, String result) {
 
     this.operand1 = operand1;
     this.operand2 = operand2;
     this.operation = operation;
     this.result = result;
+    
+    
 }
 
+//operand1 to double
 double getOperand1() {
 
-    return Double.parseDouble(operand1);
+    return Double.parseDouble(operand1.trim());
 }
-
+//operand2 to double
 double getOperand2() {
 
-    return Double.parseDouble(operand2);
+    return Double.parseDouble(operand2.trim());
 }
+// result to double
+double getResult() {
 
-double getResule() {
-
-    return Double.parseDouble(result);
+    return Double.parseDouble(result.trim());
 }
+// making operation
+void makeOperation() throws Exception {
 
-
+    if      (operation.trim().equals("+")) gotResult = getOperand1()+getOperand2();
+    else if (operation.trim().equals("-")) gotResult = getOperand1()-getOperand2();
+    else if (operation.trim().equals("*")) gotResult = getOperand1()*getOperand2();
+    else if (operation.trim().equals("/")) gotResult = getOperand1()/getOperand2();
+    else gotResult = null;
+}
 }
