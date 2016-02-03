3f7ee1cf59012b305653f65efa22cfe315f36088
b00mer <b00mer@vladb00mer>
2016-02-03 15:39:19 +0300

    less shit

--- src/test/java/b00mer/sbertechauto/autotests/GodTest.java
+++ src/test/java/b00mer/sbertechauto/autotests/GodTest.java
@@ -6,8 +6,7 @@ import java.util.List;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.*;
-        
+import static org.junit.Assert.*;        
         
 public class GodTest {
     
