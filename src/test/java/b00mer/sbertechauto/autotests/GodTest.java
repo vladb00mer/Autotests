c352c6eac99ba37136b7d647bb73056d155205d7
b00mer <b00mer@vladb00mer>
2016-02-04 10:02:39 +0300

    less shit

--- src/test/java/b00mer/sbertechauto/autotests/GodTest.java
+++ src/test/java/b00mer/sbertechauto/autotests/GodTest.java
@@ -13,6 +13,7 @@ public class GodTest {
     String filePath = "./resources/CSV/testData.csv";
     CSVFileReader csvFileReader;
     List<Expression> expList;
+    Expression exp;
     
     @Before
     public void setUp() {
@@ -27,13 +28,38 @@ public class GodTest {
         csvFileReader = null;
         expList = null;
         filePath = null;
+        exp = null;
     }
-    
-    @Test
-    public void checkForResultEquality() {
+    // for dynamic record count
+    /* @Test
+    public void checkAllRecords() {
         
         for (Expression exp: expList) {
             assertTrue (exp.getLocalResult() == exp.getResult());
         }
+    }*/
+    @Test
+    public void checkZeroRecord() {
+    
+        exp = expList.get(0);
+        assertTrue (exp.getLocalResult() == exp.getResult());
     }
-}   
+    @Test
+    public void checkFirstRecord() {
+    
+        exp = expList.get(1);
+        assertTrue (exp.getLocalResult() == exp.getResult());
+    }
+    @Test
+    public void checkSecondRecord() {
+    
+        exp = expList.get(2);
+        assertTrue (exp.getLocalResult() == exp.getResult());
+    }
+   @Test
+    public void checkThirdRecord() {
+    
+        exp = expList.get(3);
+        assertTrue (exp.getLocalResult() == exp.getResult());
+    }
+}    
