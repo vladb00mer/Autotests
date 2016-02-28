e355ad104db37640859eae16e6b12d1ea3a31b00
b00mer <b00mer@vladb00mer>
2016-02-28 08:25:09 +0300

    back to collection

--- src/test/java/b00mer/sbertechauto/autotests/ExpressionTest.java
+++ src/test/java/b00mer/sbertechauto/autotests/ExpressionTest.java
@@ -40,49 +40,16 @@ public class ExpressionTest {
         exp = null;
     }
     
-    // for dynamic record count
-    /* @Test
+    @Title("The Tests")
+    @Description("Here are the tests runs")    
+    @Test
     public void checkAllRecords() {
         
         for (Expression exp: expList) {
             checkRecord(exp.getOperand1(),exp.getOperation(),exp.getOperand2(),exp.getResult());
             
         }
-    }*/
-
-    // for fixed record count
-    @Title("First Record")
-    @Description("This is first test case")
-    @Test()
-    public void testFirstRecord() {
-    
-        exp = expList.get(0);        
-        checkRecord(exp.getOperand1(),exp.getOperation(),exp.getOperand2(),exp.getResult());
     }
-    @Title("Second Record")
-    @Description("This is second test case")
-    @Test()
-    public void testSecondRecord() {
-    
-        exp = expList.get(1);        
-        checkRecord(exp.getOperand1(),exp.getOperation(),exp.getOperand2(),exp.getResult());
-    }
-    @Title("Third Record")
-    @Description("This is third test case")
-    @Test()
-    public void testThirdRecord() {
-    
-        exp = expList.get(2);        
-        checkRecord(exp.getOperand1(),exp.getOperation(),exp.getOperand2(),exp.getResult());
-    }
-    @Title("Fourth Record")
-    @Description("This is fourth test case")
-    @Test()
-    public void testFourthRecord() {
-    
-        exp = expList.get(3);        
-        checkRecord(exp.getOperand1(),exp.getOperation(),exp.getOperand2(),exp.getResult());
-    }    
     
     @Step("Check if {0} {1} {2} = {3}")
     public void checkRecord(double a, String b, double c, double d) {
